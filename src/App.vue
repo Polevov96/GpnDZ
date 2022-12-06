21b64fbb36c1366c7f770f376e627050a83c8102
Polevov96 <polevov_96@mail.ru>
2022-12-06 11:08:44 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <WrapperTodo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WrapperTodo from './components/WrapperTodo.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WrapperTodo,
   }
 }
 </script>
 
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  margin: 0px;
+}
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
