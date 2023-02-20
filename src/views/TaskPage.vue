782545dd4544f7d9506500151ad13f7929f88025
David Chakiryan <davidchak@yandex.ru>
2023-02-20 15:56:55 +0300

    add new task page

---
+++ src/views/TaskPage.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- TODO: реализовать разметку html как в фигме, ссылка: https://www.figma.com/file/PV8ykTmSylYveoSWLgUrNI/Todo-List?node-id=402%3A218&t=2aoMFEg7Am5IOb7b-0 -->
+<div class="task">
+  <div>Task: {{ $route.params.id  }}</div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "TaskPage",
+  // TODO: реализовать логику компонента.
+};
+
+</script>
+
+<style scoped>
+/* TODO: добавить стили */
+</style>
